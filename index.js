404defd60b1bc032db8fc708a59d23c06581930c
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-03-23 23:34:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes = {}
+
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( {}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+
+function deleteFromObjectByKey(object, key) {
+  object = {}
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  object = {}
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
+  delete object.key
+  return object
+}
